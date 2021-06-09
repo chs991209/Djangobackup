b59ab83f4c6889777fa615fe3f482ebfb0f4dc66
Choi Hyunsu <chs991209@gmail.com>
2021-06-09 09:44:16 +0900

    customer, admin admin/models 구성

--- customer/models.py
+++ customer/models.py
@@ -3,17 +3,19 @@ from django.db import models
 
 # Create your models here.
 
-class Order(models.Model):
-    orderer = models.ForeignKey('customer.Customer', on_delete=models.CASCADE, verbose_name='ORDERER')
-    product = models.ForeignKey('product.Product', on_delete=models.CASCADE, verbose_name='Order_Prod_NAME')
-    quantity = models.IntegerField(verbose_name='QUANTITY')
-    registered_date = models.DateTimeField(auto_now_add=True, verbose_name='Ordered_DATE')
+class Customer(models.Model):
+    email = models.EmailField()
+    password = models.CharField(
+        max_length=64, verbose_name='PASSWORD'
+    )
+    registered_date = models.DateTimeField(auto_now_add=True,
+                                           verbose_name='CUSTOMER_REGISTERED_DATE'
+                                           )
 
     class Meta:
-        db_table = 'Shopping_Order'
-        verbose_name = 'Order'
-        verbose_name_plural = 'Order'
-
+        db_table = 'Shopping_CUSTOMER'
+        verbose_name = 'CUSTOMER'
+        verbose_name_plural = 'CUSTOMER'
 
 
 
